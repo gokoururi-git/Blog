9ab1cba139106c8f99449a8d0715a716d9b98060
caohaohao <1789551634@qq.com>
2021-08-08 23:19:54 +0800

    fix:修改了new原理的细节

--- JS/模拟实现/myNew.js
+++ JS/模拟实现/myNew.js
@@ -1,7 +1,7 @@
 function myNew(constructor, ...args){
   let obj = Object.create(constructor.prototype);// Object.create自己查一下MDN，很简单
   let res = constructor.apply(obj, args);
-  if(res !== null || typeof res === 'function' || typeof res === 'object'){
+  if(typeof res === 'object' && res !== null || typeof res === 'function'){
     //别看判断这么多，其实就是如果res是对象就返回res，否则返回obj
     return res;
   }else{
