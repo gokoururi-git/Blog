1fa5f9752a12fe543d02a6909891d7598ca22576
曹浩浩 <caohaohao.fe@bytedance.com>
2022-01-04 22:02:17 +0800

    ...

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 import Koa from 'koa';
+import KoaRouter from 'koa-router';
+import koaBody from 'koa-body';
+import koaCORS from 'koa2-cors';
+
+function timer(time) {
+  return new Promise((resolve) => { setTimeout(resolve, time) });
+}
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = '<html><body>Hello world</body></html>';
-  // ctx.res.setHeader('Access-Control-Allow-Origin', '*');
+const router = new KoaRouter();
+
+router.post('/test', async (ctx) => {
+  ctx.body = ctx.request.body.payload;
+  await timer(Math.random()*1000 % 1000);
   return ctx;
 });
 
-app.listen(80);
+router.post('/test1', async (ctx) => {
+  ctx.body = ctx.request.body.payload;
+  await timer(Math.random()*1000 % 1000);
+  return ctx;
+})
+
+app.use(koaCORS());
+app.use(koaBody());
+app.use(router.routes());
+
+
+app.listen(8080);
