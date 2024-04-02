19cb0499a5d16b10d3668e43171ee39c8d8dd8cd
caohaohao.chh <caohaohao.chh@bytedance.com>
2024-04-02 17:32:52 +0800

    feat: insert sort

--- 算法/sort/insertSort.js
+++ 算法/sort/insertSort.js
@@ -1,17 +1,21 @@
-function insertSort(nums){
-  const len = nums.length;
-  for(let i = 1; i < len; i++){
-    let index = i - 1;
-    while(nums[i] < nums[index]){//这里简写依赖了nums[-1]的值是undefined，并且任何number 和 undefined 做大小比较结果都是false
-      index--;
+function insertSort(nums, start = 0, end = nums.length - 1){
+  for (let i = start + 1; i <= end; i++) {
+    let firstSmallIndex = -1;
+    for (let comparedIndex = i - 1; comparedIndex >= start; comparedIndex--) {
+      if (nums[comparedIndex] < nums[i]) {
+        firstSmallIndex = comparedIndex;
+        break;
+      }
     }
-    let temp = nums[i];
-    for(let j = i; j > index; j--){
+    console.log([...nums.slice(0, i), '|', ...nums.slice(i)].join(' '), firstSmallIndex);
+    // 整体向右移动
+    const temp = nums[i];
+    for (let j = i; j > firstSmallIndex + 1; j--) {
       nums[j] = nums[j - 1];
     }
-    nums[index + 1] = temp;
+    nums[firstSmallIndex + 1] = temp;
   }
   return nums;
 }
 
-console.log(insertSort([5,2,3,6,9,1,4,7,8,0,11]));
+console.log(insertSort([5,2,3,6,9,1,4,7,8,11,0]));
